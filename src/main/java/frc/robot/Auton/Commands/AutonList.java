42f0ce8ef3e320366fa9f34fda4691a136ec0977
SEC-CAD-1 <SEC-CAD-1@notanemail.com>
2024-11-12 17:30:45 -0500

    right3Ball Auton Made

--- src/main/java/frc/robot/Auton/Commands/AutonList.java
+++ src/main/java/frc/robot/Auton/Commands/AutonList.java
@@ -245,7 +245,100 @@ public class AutonList{
 
     );
 
+    public static final Command right3Ball = new SequentialCommandGroup(
+        new ParallelCommandGroup(
+            new driveToDistance(1, 1, 0.1),
+            new shootToTime(Constants.shooterRPM, false, 1)
+        ),
+        new shootToTime(Constants.shooterRPM, true, 1),
+        new ParallelCommandGroup(
+            new ParallelRaceGroup(
+                new driveToRotation(27, 2),
+                new WaitCommand(1)
+            )
+            ,
+            new setIntakeExt(true)
+        ),
+        new ParallelRaceGroup(
+            new intakeBall(),
+            new driveToDistance(1.5, 1.5, 0.1),
+            new WaitCommand(3)
+        ),
+        new ParallelCommandGroup(
+            new SequentialCommandGroup(
+                new ParallelRaceGroup(
+                    new setIntakeState(-Constants.handoffPower, 0),
+                    new WaitCommand(0.15)
+                ),
+                new shootToTime(2100, false, 1)
+
+            ),
+            new ParallelRaceGroup(
+                new driveToRotation(15, 2),
+                new WaitCommand(1)
+            )
+        ),
+        new shootToTime(2100, true, 1.5),
+        new ParallelCommandGroup(
+            new ParallelRaceGroup(
+                new driveToRotation(60, 2),
+                new WaitCommand(1)
+            ),
+            new setIntakeExt(false)
+        ),
+        new driveToDistance(1.1, 1.1, 0.1),
+        new ParallelCommandGroup(
+            new ParallelRaceGroup(
+                new driveToRotation(0, 2),
+                new WaitCommand(1)
+            ),
+            new setIntakeExt(true)
+        ),
+        new ParallelRaceGroup(
+            new intakeBall(),
+            new driveToDistance(1.2, 1.2, 0.1),
+            new WaitCommand(3)
+        ),
+        new ParallelRaceGroup(
+            new intakeBall(),
+            new WaitCommand(1)
+        )
+        ,
+        new ParallelRaceGroup(
+            new driveToDistance(-1.2, -1.2, 0.1),
+            new WaitCommand(3)
+        ),
+        new ParallelCommandGroup(
+            new ParallelRaceGroup(
+                new driveToRotation(60, 2),
+                new WaitCommand(1)
+            ),
+            new setIntakeExt(false)
+        ),
+        new ParallelRaceGroup(
+            new driveToDistance(-1.1, -1.1, 0.1),
+            new WaitCommand(3)
+        ),
+        new ParallelCommandGroup(
+            new SequentialCommandGroup(
+                new ParallelRaceGroup(
+                    new setIntakeState(-Constants.handoffPower, 0),
+                    new WaitCommand(0.15)
+                ),
+                new shootToTime(2100, false, 1)
+
+            ),
+            new ParallelRaceGroup(
+                new driveToRotation(15, 2),
+                new WaitCommand(1)
+            )
+        ),
+        new shootToTime(2100, true, 1.5)
+        
+
+    );
+
     public static Optional<Command> getDefaultCommand(){
-        return Optional.ofNullable(left3Ball);
+        return Optional.ofNullable(right3Ball);
     }
 }   
